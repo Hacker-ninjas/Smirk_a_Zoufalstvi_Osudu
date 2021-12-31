31f67c9d4f0e8eb10ebf82ff8d82473f5669a718
JakeWh <kolarkuba@gmail.com>
2021-12-31 11:37:58 +0100

    .

--- program/game.py
+++ program/game.py
@@ -16,42 +16,53 @@ char = pygame.image.load("graphic/smirk/D2.png")    # výchozí obrázek postavy
 
 clock = pygame.time.Clock() # framerate hry
 
-x = 50  # výchozí bod na ose x
-y = 400  # výchozí bod na ose y
-width = 16  # šířka postavy
-height = 16 # výška postavy
-vel = 5    # počet bodů posunu
-left = False    # proměnná pohybu doleva
-right = False   # proměnná pohybu doprava
-up = False # proměnná pohybu nahoru
-down = False # proměnná pohybu dolů
-walkCount = 0   # počet snímků pohybu
-
-def redrawGameWindow(): # funkce překreslení okna při pohybu postavy
-    global walkCount    # aby změna v této proměnné byla vidět i mimo funkci
-    win.blit(bg, (0, 0)) # vyplnění okna obrázkem bg od souřadnice 0, 0
+class Player(object):
+    # třída postavy hráče
+    def __init__(self, x, y, width, height):
+        # atributy třídy
+        self.x = x  # výchozí bod na ose x
+        self.y = y  # výchozí bod na ose y
+        self.width = width  # šířka postavy
+        self.height = height    # výška postavy
+        self.vel = 5    # počet bodů posunu
+        self.left = False   # proměnná pohybu doleva
+        self.right = False  # proměnná pohybu doprava
+        self.up = False # proměnná pohybu nahoru
+        self.down = False   # proměnná pohybu dolů
+        self.walkCount = 0  # počet snímků pohybu
     
-    if walkCount + 1 >= 9: # pohyb doprava a doleva má 3 obrázků a každý se bude zobrazovat 3 snímky -> 3 * 3 = 9
-        walkCount = 0
-    if left:
-        win.blit(walkLeft[walkCount//3], (x,y))
-        walkCount += 1
-    elif right:
-        win.blit(walkRight[walkCount//3], (x,y))
-        walkCount += 1
-    elif up:
-        win.blit(walkUp[walkCount//3], (x,y))
-        walkCount += 1
-    elif down:
-        win.blit(walkDown[walkCount//3], (x,y))
-        walkCount += 1
-    else:
-        win.blit(char, (x,y))
+    def draw(self,win):
+        # definice pohybu hráče
+        if self.walkCount + 1 >= 9: # pohyb doprava a doleva má 3 obrázků a každý se bude zobrazovat 3 snímky -> 3 * 3 = 9
+            self.walkCount = 0
+        if self.left:
+            win.blit(walkLeft[self.walkCount//3], (self.x,self.y))
+            self.walkCount += 1
+        elif self.right:
+            win.blit(walkRight[self.walkCount//3], (self.x,self.y))
+            self.walkCount += 1
+        elif self.up:
+            win.blit(walkUp[self.walkCount//3], (self.x,self.y))
+            self.walkCount += 1
+        elif self.down:
+            win.blit(walkDown[self.walkCount//3], (self.x,self.y))
+            self.walkCount += 1
+        else:
+            win.blit(char, (self.x,self.y))
 
+class Enemy(object):
+    # třída postavy nepřítele
+  
+
+def redrawGameWindow():
+    # funkce překreslení okna při pohybu postavy
+    win.blit(bg, (0, 0)) # vyplnění okna obrázkem bg od souřadnice 0, 0
+    man.draw(win) 
     pygame.display.update() # obnovení displeje
 
 
 # hlavní smyčka hry
+man = Player(300, 410, 16, 16)
 run = True
 while run:
     clock.tick(21)   # framerate v FPS
@@ -60,36 +71,36 @@ while run:
             run = False
 
     keys = pygame.key.get_pressed() # definice pohybu postavy po obrazovce. Souřadnice jsou od 0,0 vlevo nahoře po 1024,768 vpravo dole. 
-    if keys [pygame.K_LEFT] and x > vel:  # pohyb doleva a ošetření uníku mimo obrazovku
-        x -= vel    # pohyb doleva odečítá na ose x rychlostí vel
-        left = True
-        right = False
-        up = False
-        down = False
-    elif keys [pygame.K_RIGHT] and x < 1024 - width - vel:   # pohyb doprava a ošetření uníku mimo obrazovku. (šiřka okna - šířka objektu - body posunu)
-        x += vel    # pohyb doprava přičítá na ose x rychlostí vel
-        right = True
-        left = False
-        up = False
-        down = False
-    elif keys [pygame.K_UP] and y > vel: # pohyb nahoru
-        y -= vel    # pohyb nahoru odečítá na ose y rychlostí vel
-        up = True
-        right = False
-        left = False
-        down = False
-    elif keys [pygame.K_DOWN] and y < 768 - width - vel:    # pohyb dolů
-        y += vel    # pohyb dolů přičítá na ose y rychlostí vel
-        down = True
-        up = False
-        left = False
-        right = False
+    if keys [pygame.K_LEFT] and man.x > man.vel:  # pohyb doleva a ošetření uníku mimo obrazovku
+        man.x -= man.vel    # pohyb doleva odečítá na ose x rychlostí vel
+        man.left = True
+        man.right = False
+        man.up = False
+        man.down = False
+    elif keys [pygame.K_RIGHT] and man.x < 1024 - man.width - man.vel:   # pohyb doprava a ošetření uníku mimo obrazovku. (šiřka okna - šířka objektu - body posunu)
+        man.x += man.vel    # pohyb doprava přičítá na ose x rychlostí vel
+        man.right = True
+        man.left = False
+        man.up = False
+        man.down = False
+    elif keys [pygame.K_UP] and man.y > man.vel: # pohyb nahoru
+        man.y -= man.vel    # pohyb nahoru odečítá na ose y rychlostí vel
+        man.up = True
+        man.right = False
+        man.left = False
+        man.down = False
+    elif keys [pygame.K_DOWN] and man.y < 768 - man.width - man.vel:    # pohyb dolů
+        man.y += man.vel    # pohyb dolů přičítá na ose y rychlostí vel
+        man.down = True
+        man.up = False
+        man.left = False
+        man.right = False
     else:   # pokud se nehýbeme
-        right = False
-        left = False
-        up = False
-        down = False
-        walkCount = 0
+        man.right = False
+        man.left = False
+        man.up = False
+        man.down = False
+        man.walkCount = 0
       
     redrawGameWindow()  # volání funkce překreslení okna
     
